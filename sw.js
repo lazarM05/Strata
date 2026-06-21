672445f4b58b5b1b47c717423fad0b2bd2e5dbd5
lazarM05 <153576841+lazarM05@users.noreply.github.com>
2026-06-21 18:07:35 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,19 @@
-const CACHE = "strata-v2";
+const CACHE = "strata-v3";
 const ASSETS = ["./strata.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function (event) {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then(function (cache) { return cache.addAll(ASSETS); }));
 });
 
 self.addEventListener("activate", function (event) {
   event.waitUntil(
-    caches.keys().then(function (keys) {
-      return Promise.all(keys.filter(function (k) { return k !== CACHE; }).map(function (k) { return caches.delete(k); }));
-    })
+    Promise.all([
+      caches.keys().then(function (keys) {
+        return Promise.all(keys.filter(function (k) { return k !== CACHE; }).map(function (k) { return caches.delete(k); }));
+      }),
+      self.clients.claim()
+    ])
   );
 });
 
